3727fa9c74fb3bcf041798968aaefc0a5ae5fff5
CarlosAlbertt <caralbort3@alu.edu.gva.es>
2025-12-28 15:35:43 +0100

    fix: Update user stats, levels (6) and ranks (D)

--- src/js/data/users.js
+++ src/js/data/users.js
@@ -16,5 +16,95 @@ export const knownUsers = {
             wis: 20,
             cha: 20
         }
+    },
+    "volibeer": {
+        rank: "D",
+        guild: "Oso de la Tormenta",
+        maxRing: 2,
+        title: "El Rugido",
+        password: "voli",
+        race: "Ursa",
+        class: "Bárbaro",
+        level: 6,
+        stats: {
+            str: 20,
+            dex: 15,
+            con: 20,
+            int: 12,
+            wis: 14,
+            cha: 8
+        }
+    },
+    "ulume": {
+        rank: "D",
+        guild: "Sombra Veloz",
+        maxRing: 2,
+        title: "Daga Silenciosa",
+        password: "ulu",
+        race: "Elfo",
+        class: "Pícaro",
+        level: 6,
+        stats: {
+            str: 8,
+            dex: 18,
+            con: 15,
+            int: 18,
+            wis: 10,
+            cha: 10
+        }
+    },
+    "m": {
+        rank: "D",
+        guild: "El Enigma",
+        maxRing: 2,
+        title: "Mente Maestra",
+        password: "eme",
+        race: "Humano",
+        class: "Estratega",
+        level: 6,
+        stats: {
+            str: 10,
+            dex: 14,
+            con: 20,
+            int: 11,
+            wis: 17,
+            cha: 10
+        }
+    },
+    "janus": {
+        rank: "D",
+        guild: "Doble Cara",
+        maxRing: 2,
+        title: "El Observador",
+        password: "jan",
+        race: "Cambiante",
+        class: "Monje",
+        level: 6,
+        stats: {
+            str: 11,
+            dex: 18,
+            con: 16,
+            int: 12,
+            wis: 17,
+            cha: 8
+        }
+    },
+    "khan": {
+        rank: "D",
+        guild: "Garra de Hierro",
+        maxRing: 2,
+        title: "Señor de las Bestias",
+        password: "khan",
+        race: "Leonin",
+        class: "Guerrero",
+        level: 6,
+        stats: {
+            str: 11,
+            dex: 20,
+            con: 16,
+            int: 12,
+            wis: 14,
+            cha: 16
+        }
     }
 };
